db48f571c808f24a8a058d4dbd1a38bd3aac8161
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-04-23 02:37:47 +0900

    Add test pattern

--- tests/test_vparser.py
+++ tests/test_vparser.py
@@ -13,7 +13,26 @@ def _set_test_data(filepath, mocker):
     mocker.patch('requests.Session.get').return_value = response_mock
 
 def test_json(mocker):
+    '''
+    The 'video' tab is at default positon (index 1).
+    '''
     _set_test_data('tests/testdata/test.json', mocker)
+    req = HttpRequest()
+    source = vparser.get_source_json(req,'dummy_channel_id')
+    vids = vparser.extract_video_ids(source)
+    assert vids[0] == 'y5BvucpirC4'
+    assert vids[1] == 'v78CVaYXRAM'
+    assert vids[2] == '9YI_edrgrU0'
+    assert vids[3] == '63YyvJrOLrc'
+    assert vids[4] == '7AS6PRs-oy8'
+    assert vids[5] == 'wz8YYFBsMHk'
+
+def test_different_tab_position(mocker):
+    '''
+    The 'Videos' tab is diffrent from default position (index 2, not 1).
+    '''
+
+    _set_test_data('tests/testdata/test_cracked.json', mocker)
     req = HttpRequest()
     source = vparser.get_source_json(req,'dummy_channel_id')
     vids = vparser.extract_video_ids(source)
