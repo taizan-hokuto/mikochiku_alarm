716372efa0b8de16b99be9297851f48c6917cf45
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-05-01 00:44:40 +0900

    Modify test

--- tests/test_vparser.py
+++ tests/test_vparser.py
@@ -1,9 +1,11 @@
 import vparser
 from httpreq import HttpRequest
 
+
 def _open_file(path):
     with open(path, mode='r', encoding='utf-8') as f:
         return f.read()
+
 
 def _set_test_data(filepath, mocker):
     _text = _open_file(filepath)
@@ -11,6 +13,7 @@ def _set_test_data(filepath, mocker):
     response_mock.status_code = 200
     response_mock.text = _text
     mocker.patch('requests.Session.get').return_value = response_mock
+
 
 def test_json(mocker):
     '''
@@ -20,12 +23,14 @@ def test_json(mocker):
     req = HttpRequest()
     source = vparser.get_source_json(req,'dummy_channel_id')
     vids = vparser.extract_video_ids(source)
-    assert vids[0] == 'y5BvucpirC4'
-    assert vids[1] == 'v78CVaYXRAM'
-    assert vids[2] == '9YI_edrgrU0'
-    assert vids[3] == '63YyvJrOLrc'
-    assert vids[4] == '7AS6PRs-oy8'
-    assert vids[5] == 'wz8YYFBsMHk'
+
+    assert vids['y5BvucpirC4'] == 'Night Jazz: Exquisite Night Jazz Playlist - Sensual Piano Jazz & Lights of Night Home for Relax'
+    assert vids['v78CVaYXRAM'] == 'My Home Jazz: Relaxing Jazz Radio & Sweet Bossa Nova for Relaxing at Home'
+    assert vids['9YI_edrgrU0'] == 'Study Cafe: Concentrate Study & Work Jazz & Bossa Nova Music for Brain Power'
+    assert vids['63YyvJrOLrc'] == 'Chill Piano: Beautiful Piano Music - Relaxing Music, Study Music, Stress Relief, Sleep Music'
+    assert vids['7AS6PRs-oy8'] == 'Nature Guitar: Easy Listening Music - Beautiful Background Guitar Music for Work, Study'
+    assert vids['wz8YYFBsMHk'] == 'Hawaiian Cafe: Relaxing Tropical Music - Hawaiian Ukulele Instrumental Music at Home'
+
 
 def test_different_tab_position(mocker):
     '''
@@ -35,9 +40,9 @@ def test_different_tab_position(mocker):
     req = HttpRequest()
     source = vparser.get_source_json(req,'dummy_channel_id')
     vids = vparser.extract_video_ids(source)
-    assert vids[0] == 'y5BvucpirC4'
-    assert vids[1] == 'v78CVaYXRAM'
-    assert vids[2] == '9YI_edrgrU0'
-    assert vids[3] == '63YyvJrOLrc'
-    assert vids[4] == '7AS6PRs-oy8'
-    assert vids[5] == 'wz8YYFBsMHk'
+    assert vids['y5BvucpirC4'] == 'Night Jazz: Exquisite Night Jazz Playlist - Sensual Piano Jazz & Lights of Night Home for Relax'
+    assert vids['v78CVaYXRAM'] == 'My Home Jazz: Relaxing Jazz Radio & Sweet Bossa Nova for Relaxing at Home'
+    assert vids['9YI_edrgrU0'] == 'Study Cafe: Concentrate Study & Work Jazz & Bossa Nova Music for Brain Power'
+    assert vids['63YyvJrOLrc'] == 'Chill Piano: Beautiful Piano Music - Relaxing Music, Study Music, Stress Relief, Sleep Music'
+    assert vids['7AS6PRs-oy8'] == 'Nature Guitar: Easy Listening Music - Beautiful Background Guitar Music for Work, Study'
+    assert vids['wz8YYFBsMHk'] == 'Hawaiian Cafe: Relaxing Tropical Music - Hawaiian Ukulele Instrumental Music at Home'
